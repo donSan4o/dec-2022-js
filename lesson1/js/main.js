edb0155674d0a91dcd8aac59b0170a1cad321825
Alexander Pogrebnyak <cah4o.pan4o@gmail.com>
2023-01-30 18:26:08 +0200

    first commit

---
+++ lesson1/js/main.js
@@ -0,0 +1,33 @@
+a = 'hello';
+b = 'owu';
+c = 'com';
+d = 'ua';
+e = 1;
+f = 10;
+g = -999;
+h = 123;
+i = 3.14;
+j = 2.7;
+k = 16;
+l = true;
+m = false;
+
+console.log(a,b,c,d,e,f,g,h,i,j,k,l,m);
+
+
+let firstName = 'Alexander';
+let middleName = 'Mykhailovich'; 
+let lastName = 'Pogrebnyak';
+let person = `${firstName} ${middleName} ${lastName}`;
+
+console.log(person);
+
+let x = 100; 
+let y = '100'; 
+let z = true;
+
+console.log(x, typeof x, y, typeof y, z, typeof z);
+
+console.log(prompt('Please, enter your Name'));
+console.log(prompt('Please, enter your Middle Name'));
+console.log(prompt('Please, enter your Last Name'));
